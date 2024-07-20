37fe8f5cbc226981913002e67e21969fc9a8f63a
miguel whyte <whytemig@gmail.com>
2024-07-20 14:20:29 -0400

    disemvowelment

---
+++ Disemvowelment/index.js
@@ -0,0 +1,26 @@
+// MY CODE.....
+
+function disemvowel(str) {
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  return str
+    .split("")
+    .filter(function (el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    })
+    .join("");
+}
+
+// CODE I LEARN....
+
+function disemvowelII(str) {
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  let newStr = [...str]
+    .filter((letter) => {
+      return !vowels.includes(letter.toLowerCase());
+    })
+    .join("");
+
+  return newStr;
+}
