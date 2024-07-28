05f132ca30a9c6c613825bd2fe5eb8bd192c3478
miguel whyte <whytemig@gmail.com>
2024-07-28 16:31:16 -0400

    add employees

--- DataManagement/index.js
+++ DataManagement/index.js
@@ -1,8 +1,9 @@
 async function getData() {
+  //fetch the data from the JSON file.
   const data = await fetch("./data.json");
   const response = await data.json();
 
-  //select a employee
+  //organizing the information that is being acquired.
   let employee = response;
   let selectedEmployeeById = employee[0].id;
   let selectedEmployee = employee[0];
@@ -10,8 +11,61 @@ async function getData() {
   let employeeNameList = document.querySelector(".employee-names-list");
   let employeeNameInfo = document.querySelector(".employee-names-info");
 
-  //selected Employee EventListener
+  // Add employee
+  const addEmployeeModal = document.querySelector(".addEmployee");
+  const createEmployeeForm = document.querySelector(".create-employee");
+  const addEmployeeBtn = document.querySelector(".addEmployeeBtn");
 
+  addEmployeeBtn.addEventListener("click", () => {
+    addEmployeeModal.style.display = "flex";
+  });
+
+  addEmployeeModal.addEventListener("click", (e) => {
+    if (e.target.className === "addEmployee") {
+      addEmployeeModal.style.display = "none";
+    }
+  });
+
+  //DOB must be over 18
+  const dobInput = document.querySelector(".addEmployee-create--dob");
+  const today = new Date();
+  const maxDate = new Date(
+    today.getFullYear() - 18,
+    today.getMonth(),
+    today.getDate()
+  );
+  // Format the date as YYYY-MM-DD to match the input date format
+  const formattedDate = maxDate.toISOString().split("T")[0];
+  dobInput.max = formattedDate;
+
+  //Calculate the Age.....................
+
+  createEmployeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(createEmployeeForm);
+
+    const values = [...formData.entries()];
+
+    let employeeObject = {};
+
+    values.forEach((value) => {
+      employeeObject[value[0]] = value[1];
+    });
+
+    employeeObject.id = employee[employee.length - 1].id + 1;
+    employeeObject.age =
+      new Date().getFullYear() - parseInt(employeeObject.dob.slice(0.4), 10);
+
+    employee.imageUrl =
+      employeeObject.imageUrl ||
+      "https://cdn-icons-png.flaticon.com/512/0/93.png";
+
+    console.log(employeeObject);
+  });
+
+  // ----------------------
+
+  //selected Employee EventListener
   employeeNameList.addEventListener("click", (e) => {
     if (e.target.tagName === "SPAN" && selectedEmployeeById !== e.target.id) {
       selectedEmployeeById = e.target.id;
@@ -20,11 +74,10 @@ async function getData() {
     }
   });
 
-  //render employees
+  //render employees within the employee div element
   const renderEmployees = () => {
     employeeNameList.innerHTML = "";
     employee.forEach((em) => {
-      //create a span element
       const employeeSpan = document.createElement("span");
       employeeSpan.classList.add("employee-names-item");
 
@@ -41,10 +94,9 @@ async function getData() {
   };
 
   //render One Single Employee
-
   function singleEmployee() {
     employeeNameInfo.innerHTML = `<img src="${selectedEmployee?.imageUrl}"/>
-     <span class="employees__single--heading">
+     <span class="employees-single--heading">
       ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
       </span>
       <span>${selectedEmployee.address}</span>
