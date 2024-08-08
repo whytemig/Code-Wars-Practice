97de68c4c46ec7de0163a927ecea657a20e8260f
miguel whyte <whytemig@gmail.com>
2024-08-07 20:30:44 -0400

    array differ

---
+++ ArrayDiffer/index.js
@@ -0,0 +1,34 @@
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+*/
+
+//My Code....
+function arrayDiff(a, b) {
+  let newArray = a; // Start with array a
+
+  for (let i = 0; i < b.length; i++) {
+    newArray = newArray.filter((num) => num !== b[i]); // Filter out elements in b
+  }
+
+  return newArray; // Return the filtered array
+}
+
+// My Code Adjusted .....
+
+function arrayDiffer(a, b) {
+  return a.filter((num) => !b.includes(num));
+}
+
+//Other People Code/......
+function array_diff(a, b) {
+  return a.filter(function (x) {
+    return b.indexOf(x) == -1;
+  });
+}
