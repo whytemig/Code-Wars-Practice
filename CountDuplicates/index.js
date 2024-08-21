3d76010dd7f5e02411d095184085a048c8a509aa
miguel whyte <whytemig@gmail.com>
2024-08-20 20:33:29 -0400

    i needed help on this one. I need to break it down first. Getting cocky

---
+++ CountDuplicates/index.js
@@ -0,0 +1,34 @@
+/*
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+*/
+
+// My Code ..........
+
+function duplicateCount(text) {
+  let map = {};
+
+  for (let letter of text.toLowerCase()) {
+    if (map[letter]) {
+      map[letter] += 1;
+    } else {
+      map[letter] = 1;
+    }
+  }
+
+  return Object.values(map).filter((val) => val > 1).length;
+}
+
+// Another......
+
+const duplicateCount = (string) => {
+  let lowerTxt = string.toLowerCase();
+  let answerArray = [];
+
+  for (let i = 0; i < lowerTxt.length; i++) {
+    if (lowerTxt[i] === lowerTxt[i + 2] && !answerArray.includes(lowerTxt[i])) {
+      answerArray.push(lowerTxt[i]);
+    }
+  }
+
+  return answerArray.length;
+};
